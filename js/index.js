f8a38b9ac835bffb4d4aaadfaca75dd820fdf3e8
Mayank Bhurani <99683515+bhurani@users.noreply.github.com>
2022-02-22 02:37:48 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
-const box = document.querySelector(".box");
+const box = document.querySelector('.box');
 
-document.addEventListener("keydown", e =>{
+document.addEventListener('keydown', e =>{
   let code = e.keyCode,
-      key = code == 32 ? "Space" : e.key;
+      key = code == 32 ? 'Space' : e.key;
 
-  box.querySelector(".key-code").innerText = code;
-  box.querySelector(".key-name").innerText = key.toUpperCase();
-  box.querySelector(".key").innerText = key;
-  box.querySelector(".code").innerText = code;
-  box.classList.add("active");
+  box.querySelector('.key-code').innerText = code;
+  box.querySelector('.key-name').innerText = key.toUpperCase();
+  box.querySelector('.key').innerText = key;
+  box.querySelector('.code').innerText = code;
+  box.classList.add('active');
 });
